07c2dd959c4f1b158facdd35c6eaee1e2082ccbb
LucaMorazzano <morazzano.1920476@studenti.uniroma1.it>
2022-07-14 22:42:48 +0200

    navbar e slideshow
    aggiunti elementi alla home

--- home.php
+++ home.php
@@ -10,50 +10,64 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		</style>
 		
 		<script>
+		var i=0;
 			function slideshow(){
-				var img =["slideshow\img1.jpg","slideshow\img2.jpg"];
-				var arr= ['1','2','3','4'];
-				var i=0; var time=0;
-						document.write(arr[i]);
-						
-						i++;
-						time=0;
+				var img =["Slideshow/img1.jpg","Slideshow/img2.jpg","Slideshow/img3.jpg","Slideshow/img4.jpg"];
 				
+				document.slider.src=img[i]; /*l'elemento slider del dom avrà src l'elemento dell'array*/
+				if(i<img.length-1)
+					i++;
+				else
+					i=0;
+				setTimeout(slideshow,3000);
 			}
+			window.onload=slideshow;
 		</script>
 	</head>
 	
 	<body>
 		<div id="header">
-			<img src="logo.png" height="35px" width="300px" alt="logo">
+			<img src="logo.png" height="35px" width="300px" alt="logo" />
 			<ul class="navbar">
-				<li><a href="">ACCOUNT<a href=""></li>
+				<nobr>
+				<?php
+				$_SESSION['login']=true;
+				if(isset($_SESSION['login'])){
+					echo "<li><a href=\"\">ACCOUNT</a></li>";
+					echo "<li><a href=\"\">CREDITI</a></li>";
+					echo "<li><a href=\"\">CARRELLO</a></li>";
+				}
+				?>
 				<li><a href="">FAQ</a></li>
-				<li><a href="">CREDITI</a></li>
-				<li><a href="">CARRELLO</a></li>
+				<?php
+					if(isset($_SESSION['login'])){
+						echo "<li><a href=\"\">LOGOUT</a></li>";
+					}
+					else{
+						echo "<li><a href=\"\">LOGIN</a></li></nobr>";
+					}
+					?>
+				</nobr>
 			</ul>
 		</div>
-		
 		<div id="main-content"> <!-- INIZIO MAIN CONTENT -->
 			<div id="left">
-		
+				
 			</div>
 		
 			<div id="center">
 			
 				<div class="slideshow">
-					<script>
-						slideshow();
-					</script>
+					<img name="slider" height="400px" width="1000px" alt="offerte" />
 				</div>
 		
 				<div class="catalogo">
-		
+					
 				</div>
 			
 			</div>
 			<div id="right">
-			
+				
 			</div>
 		</div> <!-- FINE MAIN CONTENT -->
 		
