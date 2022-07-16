b6ce93e02c957fdde6f8dec27f88b4a7eb019837
LucaMorazzano <102364382+LucaMorazzano@users.noreply.github.com>
2022-07-16 15:49:39 +0200

    Ulteriori modifiche
    problema carrello

--- carrello.php
+++ carrello.php
@@ -41,6 +41,25 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	margin-left:auto;
 	margin-right:auto;
 }
+.giochi{
+	list-style-type:none;
+}
+.giochi li{
+	display:inline;
+}
+.giochi li input{
+	background-color: #a61022;
+	color: white;
+	font-size: 15px;
+	font-family:Arial;
+	padding: 10px 10px;
+	border: none;
+	border-radius: 5px;
+	opacity:75%;
+}
+.giochi li input:hover{
+	opacity:100%;
+}
 </style>
 	</head>
 	
@@ -48,9 +67,8 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		<?php
 			session_start();
 			if(isset($_SESSION['login'])){ //se siamo loggati
-				require_once("connection.php");
 		?>
-		<div id="header">
+		<div id="heade">
 			<a style="padding-left:2%" href="homepage.php"><img src="logo.png" height="35px" width="300px" alt="logo" /></a>
 			<ul class="navbar">
 				<nobr>
@@ -64,12 +82,28 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 		</div>
 		<div id="carrello">
 			<?php
-				$carrello=$_SESSION['carrello'];
-				for($i=0;$i<sizeof($carrello); $i++){
+				echo "<form action=\"rimuovi.php\" method=\"POST\" >";
+				echo "<ul class=\"giochi\">";
+				foreach($_SESSION['carrello'] as $gioco){
 					//estraiamo informazioni su elementi presenti nel carrello
-					$nome=$carrello[$i]->firstChild;
+					$id=$gioco->getAttribute('id');
+					$nome=$gioco->firstChild;
 					$nomevalue=$nome->textContent;
+					$immagine= $nome->nextSibling;
+					$immaginevalue= $immagine->textContent;
+					$tipologia= $immagine->nextSibling;
+					$tipologiavalue=$tipologia->textContent;
+					$prezzo=$tipologia->nextSibling;
+					$prezzovalue=(double)($prezzo->textContent); //casting necessario
+					$data= $prezzo->nextSibling;
+					//estraiamo gli attributi della data di uscita
+					$giorno= $data->getAttribute('gg');
+					$mese= $data->getAttribute('mm');
+					$anno= $data->getAttribute('aaaa');
+					echo"<li><img src=\"$immaginevalue\" height=\"170px\" width=\"150px\" alt=\"game\" />$nomevalue $prezzovalue 
+							<input type=\"submit\" name=\"id\" value=\"$id\"></li>";	/*bottone per rimuovere elemento dal carrello*/				
 				}
+				echo "</ul></form>";
 			
 			?>
 		
